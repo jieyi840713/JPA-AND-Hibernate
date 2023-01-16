4a9f2d2c57ddd752bb34118d16742cbe7b0cdc13
Jay <jieyi840713@gmail.com>
2023-01-16 12:16:07 +0800

    Getting start with JPA and EntityManager

--- src/main/java/selfstudy/learnjpaandhibernate/course/Course.java
+++ src/main/java/selfstudy/learnjpaandhibernate/course/Course.java
@@ -1,10 +1,21 @@
 package selfstudy.learnjpaandhibernate.course;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity (name="Course_Details")
 public class Course {
+    @Id
     private long id;
+    @Column(name="name")
     private String name;
+    @Column(name="author")
     private String author;
+
+    public Course() {
+
+    }
 
     public Course(long id, String name, String author) {
         this.id = id;
--- src/main/java/selfstudy/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/selfstudy/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,15 +1,19 @@
-package selfstudy.learnjpaandhibernate.course.jdbc;
+package selfstudy.learnjpaandhibernate.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import selfstudy.learnjpaandhibernate.course.Course;
+import selfstudy.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
+import selfstudy.learnjpaandhibernate.course.jpa.CourseJPARepository;
 
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+public class CourseCommandLineRunner implements CommandLineRunner {
+
+//    @Autowired
+//    private CourseJdbcRepository repository;
 
     @Autowired
-    private CourseJdbcRepository repository;
+    private CourseJPARepository repository;
     @Override
     public void run(String... args) throws Exception {
         repository.insert(new Course(1, "Learn AWS now!", "in28minutes"));
@@ -17,7 +21,7 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
         repository.insert(new Course(3, "Learn DevOps now!", "in28minutes"));
 
         repository.deleteById(1);
-
+        System.out.println(repository.findById(2));
         System.out.println(repository.findById(3));
     }
 }
