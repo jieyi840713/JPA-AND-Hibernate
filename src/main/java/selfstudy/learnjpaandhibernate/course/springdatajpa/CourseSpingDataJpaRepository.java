54328f7ec3103b821ce809d02e25d7f1736bc4db
Jay <jieyi840713@gmail.com>
2023-01-16 12:43:29 +0800

    Exploring features of Spring Data Jpa

--- src/main/java/selfstudy/learnjpaandhibernate/course/springdatajpa/CourseSpingDataJpaRepository.java
+++ src/main/java/selfstudy/learnjpaandhibernate/course/springdatajpa/CourseSpingDataJpaRepository.java
@@ -3,6 +3,10 @@ package selfstudy.learnjpaandhibernate.course.springdatajpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import selfstudy.learnjpaandhibernate.course.Course;
 
+import java.util.List;
+
 public interface CourseSpingDataJpaRepository extends JpaRepository<Course, Long> {
 
+    List<Course> findByAuthor(String author);
+    List<Course> findByName(String name);
 }
